25ded4d5427958518478917a5c6c4cbc439574bd
rlot <rj23@protonmail.com>
2017-02-12 15:44:41 +0100

    Fixed support to MW 1.28
    MW1.28 does not support OO.ui.prompt()
    
    Bug: T155451
    Change-Id: I76ffa3bbb622c7aecc95ec8c63c4601756fe3307

--- js/QuizGame.js
+++ js/QuizGame.js
@@ -6,6 +6,39 @@
  * @author Jack Phoenix <jack@countervandalism.net>
  * @date 8 October 2014
  */
+
+ /*
+	* That needs to be removed, when we drop support to MW 1.28. Modified copy pasta from OOjsUI windows.js
+	* @see https://gerrit.wikimedia.org/r/#/c/336008/
+	*/
+reasonPrompt = function ( text, options ) {
+	manager = new OO.ui.WindowManager();
+	textInput = new OO.ui.TextInputWidget( ( options && options.textInput ) || {} );
+	textField = new OO.ui.FieldLayout( textInput, {
+		align: 'top',
+		label: text
+	} );
+	$( 'body' ).append( manager.$element );
+	manager.addWindows( [ new OO.ui.MessageDialog() ] );
+
+	// TODO: This is a little hacky, and could be done by extending MessageDialog instead.
+
+	return manager.openWindow( 'message', $.extend( {
+		message: textField.$element
+	}, options ) ).then( function ( opened ) {
+		// After ready
+		textInput.on( 'enter', function () {
+			manager.getCurrentWindow().close( { action: 'accept' } );
+		} );
+		textInput.focus();
+		return opened.then( function ( closing ) {
+			return closing.then( function ( data ) {
+				return $.Deferred().resolve( data && data.action === 'accept' ? textInput.getValue() : null );
+			} );
+		} );
+	} );
+};
+
 window.QuizGame = {
 	continue_timer: '', // has to have an initial value...
 	voted: 0,
@@ -237,7 +270,7 @@ window.QuizGame = {
 			],
 			textInput: { placeholder: mw.msg( 'quizgame-flagged-reason' ) }
 		};
-		OO.ui.prompt( mw.msg( 'quizgame-flag-confirm' ), options ).done( function ( reason ) {
+		reasonPrompt( mw.msg( 'quizgame-flag-confirm' ), options ).done( function ( reason ) {
 			if ( reason !== null ) {
 				var gameKey = document.getElementById( 'quizGameKey' ).value;
 				var gameId = document.getElementById( 'quizGameId' ).value;
