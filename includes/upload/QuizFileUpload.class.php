d043ce9cdd54479bd48b71647686f1e0f4039311
Umherirrender <umherirrender_de.wp@web.de>
2024-11-15 14:23:57 +0100

    Use explicit nullable type on parameter arguments (for PHP 8.4)
    Implicitly marking parameter $... as nullable is deprecated in PHP
    8.4. The explicit nullable type must be used instead.
    
    Change-Id: If3fb704e52bef639e19bf8e290aa333a891f31cb

--- includes/upload/QuizFileUpload.class.php
+++ includes/upload/QuizFileUpload.class.php
@@ -26,7 +26,7 @@ class QuizFileUpload extends UploadFromFile {
 		$this->initialize( $desiredDestName, $upload );
 	}
 
-	public function doStashFile( User $user = null ) {
+	public function doStashFile( ?User $user = null ) {
 		return parent::doStashFile( $user );
 	}
 }
